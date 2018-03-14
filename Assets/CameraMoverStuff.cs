3076e6d0aec51160f4da5a7874ffa705a0cb5deb
TheFlashBold <tycho.holzer@gmail.com>
2018-03-14 21:13:38 +0100

    first commit

---
+++ Assets/CameraMoverStuff.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoverStuff : MonoBehaviour {
+
+    public GameObject target;
+    public float speedMod = 10.0f;
+    public float minScale = 0.01f;
+    public float maxScale = 1f;
+
+    private Vector3 point;
+
+    void Start()
+    {
+        point = target.transform.position;
+        transform.LookAt(point);
+    }
+
+    void Update()
+    {
+        transform.RotateAround(target.transform.position, Vector3.up, speedMod * Time.deltaTime);
+
+        float zoomValue = Input.GetAxis("Mouse ScrollWheel");
+
+        if (zoomValue != 0)
+        {
+            target.transform.localScale += Vector3.one * zoomValue;
+            target.transform.localScale = Vector3.Max(target.transform.localScale, new Vector3(minScale, minScale, minScale));
+            target.transform.localScale = Vector3.Min(target.transform.localScale, new Vector3(maxScale, maxScale, maxScale));
+        }
+    }
+}
---
+++ Assets/LoadOBJOnStartup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LoadOBJOnStartup : MonoBehaviour {
+    
+    string[] args;
+    
+    void Start () {
+        args = Environment.GetCommandLineArgs();
+        if (args.Length > 1)
+        {
+            GameObject g = OBJLoader.LoadOBJFile(args[1]);
+            g.transform.localScale = new Vector3(0.07f, 0.07f, 0.07f);
+            g.transform.SetParent(this.transform);
+        }
+        else
+        {
+            GameObject g = OBJLoader.LoadOBJFile("GEO_Trolley0.03.obj");
+            g.transform.localScale = new Vector3(0.07f, 0.07f, 0.07f);
+            g.transform.SetParent(this.transform);
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (args.Length > 1)
+        {
+            GUI.Box(new Rect(0, 0, Screen.width, 20), args[1]);
+        }
+    }
+}
